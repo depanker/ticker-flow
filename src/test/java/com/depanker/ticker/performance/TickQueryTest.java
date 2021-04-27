a2597849beaf1ed7cb5d154c60d5161a0f707782
depanker <depanker@gmail.com>
2021-04-27 18:56:47 +0530

    Added code to warmup

--- src/test/java/com/depanker/ticker/performance/TickQueryTest.java
+++ src/test/java/com/depanker/ticker/performance/TickQueryTest.java
@@ -1,5 +1,6 @@
 package com.depanker.ticker.performance;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import us.abstracta.jmeter.javadsl.core.TestPlanStats;
 
@@ -11,6 +12,17 @@ import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
 
 public class TickQueryTest {
 
+    @BeforeEach
+    public void setup() throws IOException {//Warmup
+        testPlan(
+                threadGroup(1, 10,
+                        httpSampler("http://localhost:8080/lookup?query=select * from AAPL.OQ where CLOSE_PRICE != null")
+                                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmMifQ.zqQixZbzz1sQH_GuspsNb0GozyrCFJ5qO2wSm4sY_w1z9WAmRy47XtMdU2DMrs8ucVnLFvFxvHGwblnNHdFSOw")
+
+                )
+        ).run();
+    }
+
     @Test
     public void testPerformance() throws IOException {
         //this is just to log details of each request stats
@@ -19,11 +31,11 @@ public class TickQueryTest {
                         httpSampler("http://localhost:8080/lookup?query=select * from AAPL.OQ where CLOSE_PRICE != null")
                                 .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmMifQ.zqQixZbzz1sQH_GuspsNb0GozyrCFJ5qO2wSm4sY_w1z9WAmRy47XtMdU2DMrs8ucVnLFvFxvHGwblnNHdFSOw")
 
-                )
+                )/*,
                 //this is just to log details of each request stats
-//                jtlWriter("test-query.csv")
+                jtlWriter("test-query.csv")*/
         ).run();
         assertThat(stats.overall().errorsCount()).isEqualTo(0l);
-        assertThat(stats.overall().elapsedTimePercentile90()).isLessThan(Duration.ofMillis(100));
+        assertThat(stats.overall().elapsedTimePercentile90()).isLessThan(Duration.ofMillis(40));
     }
 }
